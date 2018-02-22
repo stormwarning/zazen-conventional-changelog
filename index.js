ba0002681cf71eeecfe678a4cb4386fa4b47f6c3
Jeff <rustyangel@gmail.com>
2018-02-22 13:45:41 -0700

    🎁 Add preset files based on "Atom" preset
    See: https://github.com/conventional-changelog/conventional-changelog/tree/master/packages/conventional-changelog-atom

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const Q = require(`q`)
+
+const conventionalChangelog = require(`./conventional-changelog`)
+const recommendedBumpOpts = require(`./conventional-recommended-bump`)
+const parserOpts = require(`./parser-opts`)
+const writerOpts = require(`./writer-opts`)
+
+function presetOpts (cb) {
+    Q.all([
+        conventionalChangelog,
+        parserOpts,
+        recommendedBumpOpts,
+        writerOpts,
+    ]).spread(
+        (
+            conventionalChangelog,
+            parserOpts,
+            recommendedBumpOpts,
+            writerOpts,
+        ) => {
+            cb(null, {
+                conventionalChangelog,
+                parserOpts,
+                recommendedBumpOpts,
+                writerOpts,
+            })
+        },
+    )
+}
+
+module.exports = presetOpts
---
+++ parser-opts.js
@@ -0,0 +1,6 @@
+'use strict'
+
+module.exports = {
+    headerPattern: /^(:.*?:) (.*)$/,
+    headerCorrespondence: [`emoji`, `shortDesc`],
+}
