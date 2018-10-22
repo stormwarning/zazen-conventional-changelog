c72804cbb3898ddf5da8659287e889082aeb8059
Jeff <rustyangel@gmail.com>
2018-10-22 12:25:02 -0600

    ⚙️ Fix eslint parsing error with trailing commas in function args

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
     extends: '@zazen/eslint-config',
-    rules: {},
+    rules: {
+        'comma-dangle': [
+            'error',
+            {
+                arrays: 'always-multiline',
+                objects: 'always-multiline',
+                imports: 'always-multiline',
+                exports: 'always-multiline',
+                functions: 'ignore',
+            },
+        ],
+    },
 }
