bfcd28ec35dce46a464acd2963b9684da2813b5b
Jeff <rustyangel@gmail.com>
2018-11-05 12:48:39 -0700

    🐛 Fix error in headerPattern regex
    Scope chunk should be optional.

--- parser-opts.js
+++ parser-opts.js
@@ -1,7 +1,13 @@
 'use strict'
 
+/**
+ * Requires Unicode-aware regex in ES6. Fallbacks are:
+ *
+ * headerPattern: /^((?:[\0-\t\x0B\f\x0E-\u2027\u202A-\uD7FF\uE000-\uFFFF]|[\uD800-\uDBFF][\uDC00-\uDFFF]|[\uD800-\uDBFF](?![\uDC00-\uDFFF])|(?:[^\uD800-\uDBFF]|^)[\uDC00-\uDFFF]){1})(?:\((.*)\):)? (.*)$/,
+ * revertPattern: /^\u23EA\s([\s\S]*?)\s*This reverts commit (\w*)\./,
+ */
 module.exports = {
-    headerPattern: /^(.{1})(?:\((.*)\))?: (.*)$/u,
+    headerPattern: /^(.{1})(?:\((.*)\):)? (.*)$/u,
     headerCorrespondence: [`emoji`, `scope`, `message`],
     noteKeywords: [`BREAKING CHANGE`, `BREAKING CHANGES`, `💣`],
     revertPattern: /^⏪\s([\s\S]*?)\s*This reverts commit (\w*)\./u,
