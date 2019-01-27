e9ec013b03c3181871482291268af39130e82a73
Jeff <rustyangel@gmail.com>
2019-01-26 21:48:42 -0700

    Update dependency versions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,4 @@
 module.exports = {
     extends: '@zazen/eslint-config',
-    rules: {
-        'comma-dangle': [
-            'error',
-            {
-                arrays: 'always-multiline',
-                objects: 'always-multiline',
-                imports: 'always-multiline',
-                exports: 'always-multiline',
-                functions: 'ignore',
-            },
-        ],
-    },
+    rules: {}
 }
